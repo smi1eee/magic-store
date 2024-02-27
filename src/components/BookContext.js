3b8eb890ff8953a8a68628862957fa7a6f98215b
smi1eee <oleksandr.tk7@gmail.com>
2024-02-28 00:16:08 +0200

    initial commit

--- src/components/BookContext.js
+++ src/components/BookContext.js
@@ -7,7 +7,7 @@ export const BookProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    fetch('magic-store/books.json')
+    fetch('magic-store/public/books.json')
       .then(response => response.json())
       .then(data => {
         setBooks(data.books);
