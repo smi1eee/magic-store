2e9d1fe156af64244c3b86693ddb6f65a2f2b498
smi1eee <oleksandr.tk7@gmail.com>
2024-02-29 20:57:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Routes, Navigate, Outlet } from 'react-router-dom'; 
+import { BrowserRouter, Route, Routes, Navigate, Outlet } from 'react-router-dom'; 
 import SignIn from './components/SignIn';
 import BookList from './components/BookList';
 import SpecificBook from './components/SpecificBook';
@@ -23,7 +23,7 @@ function App() {
   
   
   return (
-    <HashRouter basename={process.env.PUBLIC_URL} hashType="noslash">
+    <BrowserRouter basename="/magic-store">
       <BookProvider>
         <Header /> 
         <Routes>
@@ -39,7 +39,7 @@ function App() {
         </Routes>
         <Footer />
       </BookProvider>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
